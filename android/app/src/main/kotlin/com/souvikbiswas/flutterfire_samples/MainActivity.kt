49054e43533817d203dce3efc399d6f52dd0a5a2
Soham Samanta <72293985+soham-samanta@users.noreply.github.com>
2021-10-01 18:38:51 +0530

    adding splash screen

--- android/app/src/main/kotlin/com/souvikbiswas/flutterfire_samples/MainActivity.kt
+++ android/app/src/main/kotlin/com/souvikbiswas/flutterfire_samples/MainActivity.kt
@@ -4,3 +4,25 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+          
+        // This is used to hide the status bar and make 
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+  
+        // we used the postDelayed(Runnable, time) method 
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 3000 is the delayed time in milliseconds.
+    }
+}
